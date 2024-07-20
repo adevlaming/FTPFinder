4fc5da61ffa6c13141cf811e302b3141d49e3d86
Ivan <ivhega@gmail.com>
2024-07-20 11:43:52 -0600

    Welcome page CSS

--- app/page.js
+++ app/page.js
@@ -1,14 +1,17 @@
 import Link from "next/link";
 
-
 export default function Home() {
   return (
-    <main>
-      <h1>Game Finder</h1>
-      <p><Link href="./finder">Let's Play!</Link></p>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-black text-center text-orange-500">
+      <h1 className="text-4xl mb-4">Game Finder</h1>
+      <Link href="./finder">
+        <button className="bg-orange-500 text-white py-2 px-4 rounded-lg hover:bg-orange-400 transition-colors focus:outline-none focus:ring-2 focus:ring-orange-300">
+          Let's Play!
+        </button>
+      </Link>
     </main>
   );
-};
+}
 
 /*
 https://www.freetogame.com/api-doc
